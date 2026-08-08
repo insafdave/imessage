e614cb94a909adb1530abef770557fe6077f67ec
insafdave <insaffizu@gmail.com>
2026-08-08 04:14:21 +0300

    Add Dockerfile and .dockerignore for containerization setup

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,11 @@ import cors from "cors";
 
 import "dotenv/config";
 
+import fs from "fs";
+import path from "path";
+
+
+
 import { clerkMiddleware } from '@clerk/express';
 
 import User from "./models/User.js";
@@ -12,6 +17,8 @@ const app = express();
 const PORT = process.env.PORT;
 const FRONTEND_URL = process.env.FRONTEND_URL;
 
+const publicDir = path.join(path.cwd(), "public");
+
 app.use(express.json());
 app.use(cors({origin:FRONTEND_URL, credentials:true}));
 app.use(clerkMiddleware());
@@ -19,6 +26,14 @@ app.use(clerkMiddleware());
 app.get("/health", (req,res) => {
     res.status(200).json({ ok: true });
 });
+
+if(fs.existsSync(publicDir)) {
+    app.use(express.static(publicDir));
+
+    app.get("/{*any}", (req,res,next) => {
+        res.sendFile(path.join(publicDir, "index.html"), (err) => next(err));
+});
+}
 
 app.listen(PORT, () => {
     connectDB();
