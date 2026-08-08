3466a8e43d5e1b1d0ff2503d0c30395a864a1c46
insafdave <insaffizu@gmail.com>
2026-08-08 08:38:56 +0300

    feat: implement Clerk webhook for user creation and deletion events

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,12 +12,17 @@ import { clerkMiddleware } from '@clerk/express';
 
 import User from "./models/User.js";
 import { connectDB } from "./lib/db.js";
+import job from "./lib/job.js";
+
+import clerkWebhook from "./webhooks/clerk.webhook.js";
 
 const app = express();
 const PORT = process.env.PORT;
 const FRONTEND_URL = process.env.FRONTEND_URL;
 
 const publicDir = path.join(process.cwd(), "public");
+
+app.use("/api/webhooks/clerk", express.raw({ type: "application/json" }), clerkWebhook);
 
 app.use(express.json());
 app.use(cors({origin:FRONTEND_URL, credentials:true}));
