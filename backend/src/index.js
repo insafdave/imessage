440594a61c90a1dbefc3106533a5534137a227c7
insafdave <insaffizu@gmail.com>
2026-08-09 22:45:28 +0300

    fix: set default PORT value and improve server startup log message

--- backend/src/index.js
+++ backend/src/index.js
@@ -20,7 +20,7 @@ import messageRoutes from "./routes/message.route.js";
 import { server } from "./lib/socket.js";
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 const FRONTEND_URL = process.env.FRONTEND_URL;
 
 const publicDir = path.join(process.cwd(), "public");
@@ -31,8 +31,12 @@ app.use(express.json());
 app.use(cors({origin:FRONTEND_URL, credentials:true}));
 app.use(clerkMiddleware());
 
+
+app.get("/", (req, res) => {
+    res.json({ message: "Backend is running..."});
+});
 app.get("/health", (req,res) => {
-    res.status(200).json({ ok: true });
+    res.json({ ok: true });
 });
 
 app.use("/api/auth",authRoutes);
@@ -48,7 +52,7 @@ if(fs.existsSync(publicDir)) {
 
 server.listen(PORT, () => {
     connectDB();
-    console.log("Server is up and running on PORT:", PORT);
+    console.log(`Server running on port ${PORT}`);
 
     if(process.env.NODE_ENV === "production");
 });
