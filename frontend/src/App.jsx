e63d418dc98cd0f1645e00989fcd9969171707b8
insafdave <insaffizu@gmail.com>
2026-08-10 02:26:55 +0300

    feat: add axios and react-hot-toast dependencies, implement useAuthStore for authentication management

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { ThemeProvider } from "./context/ThemeContext";
 import { WallpaperProvider } from "./context/WallPapersContext";
 import { Routes, Route, Navigate } from 'react-router';
@@ -6,15 +6,30 @@ import ChatPage from './pages/ChatPage';
 import AuthPage from './pages/AuthPage';
 import { useAuth } from "@clerk/react";
 import PageLoader from './components/PageLoader';
+import { useAuthStore } from './store/useAuthStore';
 
+import { Toaster } from "react-hot-toast";
 
 function App() {
-
   const [count, setCount] = useState(0);
   const {isSignedIn, isLoaded } = useAuth();
 
+  //option 1
+  //const {checkAuth,isCheckingAuth,clearAuth} = useAuthStore();
 
-  if (!isLoaded) return <PageLoader />;
+  //option 2 - better for perfomance
+  const clearAuth = useAuthStore((state) => state.clearAuth);
+  const checkAuth = useAuthStore((state) => state.checkAuth);
+  const isCheckingAuth = useAuthStore((state) => state.isCheckingAuth);
+
+  useEffect(() => {
+    if (!isLoaded) return;
+
+    if (isSignedIn) checkAuth();
+    else clearAuth();
+  },[checkAuth,clearAuth,isLoaded,isSignedIn]);
+
+  if (!isLoaded || (isSignedIn && isCheckingAuth)) return <PageLoader />;
 
   return (
     <ThemeProvider>
@@ -24,6 +39,7 @@ function App() {
           <Route path="/auth" element={!isSignedIn ? <AuthPage /> : <Navigate to={"/"} replace />} />
 
         </Routes>
+        <Toaster />
        </WallpaperProvider>
     </ThemeProvider>
   )
