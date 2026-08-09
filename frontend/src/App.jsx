eab36bf5131527b8ef08d1a658694395b5d6e140
insafdave <insaffizu@gmail.com>
2026-08-09 22:02:10 +0300

    feat: implement theme and wallpaper context providers
    - Added ThemeContext and WallpaperContext to manage application themes and wallpapers.
    - Created herouiThemePresets.js and wallpapers.js to store theme presets and wallpaper data.
    - Introduced ThemeProvider and WallpaperProvider components for context management.
    - Updated main.jsx to include BrowserRouter for routing.
    - Created AuthPage and ChatPage components for future routing.
    - Removed vite.svg asset as it is no longer needed.
    - Updated index.css to integrate Tailwind CSS and HeroUI styles.
    - Configured Vite to use Tailwind CSS and Babel for React.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,29 @@
-import './App.css';
-import { Show, SignInButton, SignUpButton, UserButton } from '@clerk/react';
 import { useState } from 'react';
+import { ThemeProvider } from "./context/ThemeContext";
+import { WallpaperProvider } from "./context/WallPapersContext";
+import { Routes, Route, Navigate } from 'react-router';
+import ChatPage from './pages/ChatPage';
+import AuthPage from './pages/AuthPage';
+import { useAuth } from "@clerk/react";
+
+
 function App() {
+
   const [count, setCount] = useState(0);
+  const {isSignedIn, isLoaded } = useAuth();
+
+  if (!isLoaded) return <p>loading...</p>;
 
   return (
-    <div>
-      <h1>MY APP</h1>
+    <ThemeProvider>
+    <WallpaperProvider>
+        <Routes>
+          <Route path="/" element={isSignedIn ? <ChatPage /> : <Navigate to={"/auth"} replace />} />
+          <Route path="/auth" element={!isSignedIn ? <AuthPage /> : <Navigate to={"/chat"} replace />} />
 
-      <header>
-        <Show when="signed-out">
-          <SignInButton mode="modal" />
-          <SignUpButton mode="modal" />
-        </Show>
-        <Show when="signed-in">
-          <UserButton />
-        </Show>
-      </header>
-    </div>
+        </Routes>
+       </WallpaperProvider>
+    </ThemeProvider>
   )
 };
 
